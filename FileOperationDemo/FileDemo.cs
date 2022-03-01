5deba2e486ecbc5d14c2069add5da071e5daf5c6
SantoshThakare <santoshthakare50@gmail.com>
2022-03-01 21:56:00 +0530

    File exist

---
+++ FileOperationDemo/FileDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperationDemo
+{
+    public class FileDemo
+    {
+        public  void FileExists()
+        {
+            String path = @"E:\VSCode\BasicProgram\FileOperationDemo\Sample.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File exists");
+            }
+        }
+    }
+}
---
+++ FileOperationDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperationDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileDemo file = new FileDemo();
+            file.FileExists();
+
+        }
+    }
+
+}
